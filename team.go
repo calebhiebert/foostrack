8094d08f9309eedbcd106ab7d342071a9bc12ba6
Caleb Hiebert <calebkhiebert@gmail.com>
2019-05-13 12:22:53 -0500

    team form basic validation

--- team.go
+++ team.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-// GetTeamForm returns the form required to create a team
-func GetTeamForm(c *gin.Context) {
-
+func renderTeamForm(c *gin.Context, data gin.H) {
 	var users []User
 
 	if err := dbase.Find(&users).Error; err != nil {
@@ -16,9 +16,14 @@ func GetTeamForm(c *gin.Context) {
 		return
 	}
 
-	SendHTML(http.StatusOK, c, "teamform", gin.H{
-		"users": users,
-	})
+	data["users"] = users
+
+	SendHTML(http.StatusOK, c, "teamform", data)
+}
+
+// GetTeamForm returns the form required to create a team
+func GetTeamForm(c *gin.Context) {
+	renderTeamForm(c, gin.H{})
 }
 
 // PostCreateTeam will accept input from the create team form and
@@ -26,6 +31,22 @@ func GetTeamForm(c *gin.Context) {
 func PostCreateTeam(c *gin.Context) {
 	name := c.PostForm("name")
 	members := c.PostFormArray("members")
+
+	name = strings.TrimSpace(name)
+
+	if len(members) != 2 {
+		renderTeamForm(c, gin.H{
+			"errors": []error{errors.New("A team must have 2 members")},
+		})
+		return
+	}
+
+	if name == "" {
+		renderTeamForm(c, gin.H{
+			"errors": []error{errors.New("A team cannot have an empty name")},
+		})
+		return
+	}
 
 	// Create new transaction
 	tx := dbase.Begin()
